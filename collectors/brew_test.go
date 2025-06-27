8419058b0c1c5a54a09bd6c1a36c7e7d785f5e7b
ajay.panthagani <ajay.panthagani@ninjavan.co>
2025-06-27 18:19:36 +0530

    add tests for error cases in brew collector

--- collectors/brew_test.go
+++ collectors/brew_test.go
@@ -2,6 +2,7 @@ package collectors_test
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/ajaypanthagani/sugabred/collectors"
@@ -43,29 +44,99 @@ var _ = Describe("Brew Collector", func() {
 		brewCollector = collectors.NewBrewCollector(mockBrewCommander)
 	})
 
-	It("collects brew packages correctly", func() {
-		mockBrewCommander.EXPECT().RunBrewListFormula().Return(mockBrewFormulae, nil)
-		mockBrewCommander.EXPECT().RunBrewInfoJSON("git", false).Return(mockBrewInfoMap["git"], nil)
-		mockBrewCommander.EXPECT().RunBrewInfoJSON("go", false).Return(mockBrewInfoMap["go"], nil)
+	Describe("CollectPackages", func() {
+		When("error retrieving formula list", func() {
+			It("returns error", func() {
+				expectedErr := fmt.Errorf("error retrieving brew formula list")
+				mockBrewCommander.EXPECT().RunBrewListFormula().Return(nil, expectedErr)
 
-		pkgs, err := brewCollector.CollectPackages()
+				pkgs, err := brewCollector.CollectPackages()
 
-		Expect(err).ToNot(HaveOccurred())
-		Expect(pkgs).To(ConsistOf(
-			types.BrewPackage{Name: "git", Version: "2.44.0"},
-			types.BrewPackage{Name: "go", Version: "1.22.1"},
-		))
+				Expect(err).To(HaveOccurred())
+				Expect(err).To(Equal(expectedErr))
+				Expect(pkgs).To(BeNil())
+			})
+		})
+
+		When("error retrieving brew info for a package", func() {
+			It("returns remaining packages skipping erroneous package", func() {
+				expectedErr := fmt.Errorf("error retrieving brew info for package")
+
+				mockBrewCommander.EXPECT().RunBrewListFormula().Return(mockBrewFormulae, nil)
+				mockBrewCommander.EXPECT().RunBrewInfoJSON("git", false).Return(nil, expectedErr)
+				mockBrewCommander.EXPECT().RunBrewInfoJSON("go", false).Return(mockBrewInfoMap["go"], nil)
+
+				pkgs, err := brewCollector.CollectPackages()
+
+				Expect(err).ToNot(HaveOccurred())
+				Expect(pkgs).To(ConsistOf(
+					types.BrewPackage{Name: "go", Version: "1.22.1"},
+				))
+				Expect(pkgs).ToNot(ConsistOf(
+					types.BrewPackage{Name: "git", Version: "2.44.0"},
+				))
+			})
+		})
+
+		When("successfully collects brew packages", func() {
+			It("returns collected packages", func() {
+				mockBrewCommander.EXPECT().RunBrewListFormula().Return(mockBrewFormulae, nil)
+				mockBrewCommander.EXPECT().RunBrewInfoJSON("git", false).Return(mockBrewInfoMap["git"], nil)
+				mockBrewCommander.EXPECT().RunBrewInfoJSON("go", false).Return(mockBrewInfoMap["go"], nil)
+
+				pkgs, err := brewCollector.CollectPackages()
+
+				Expect(err).ToNot(HaveOccurred())
+				Expect(pkgs).To(ConsistOf(
+					types.BrewPackage{Name: "git", Version: "2.44.0"},
+					types.BrewPackage{Name: "go", Version: "1.22.1"},
+				))
+			})
+		})
 	})
 
-	It("collects brew casks correctly", func() {
-		mockBrewCommander.EXPECT().RunBrewListCask().Return(mockBrewCasks, nil)
-		mockBrewCommander.EXPECT().RunBrewInfoJSON("chrome", true).Return(mockBrewInfoMap["chrome"], nil)
+	Describe("CollectCasks", func() {
+		When("error retrieving cask list", func() {
+			It("returns error", func() {
+				expectedErr := fmt.Errorf("error retrieving brew formula list")
+				mockBrewCommander.EXPECT().RunBrewListCask().Return(nil, expectedErr)
 
-		casks, err := brewCollector.CollectCasks()
-		Expect(err).ToNot(HaveOccurred())
-		Expect(casks).To(ConsistOf(
-			types.BrewCask{Name: "chrome", Version: "125.0"},
-		))
+				casks, err := brewCollector.CollectCasks()
+
+				Expect(err).To(HaveOccurred())
+				Expect(err).To(Equal(expectedErr))
+				Expect(casks).To(BeNil())
+			})
+		})
+
+		When("error retrieving brew info for a cask", func() {
+			It("skipps erroneous cask in result", func() {
+				expectedErr := fmt.Errorf("error retrieving brew formula list")
+
+				mockBrewCommander.EXPECT().RunBrewListCask().Return(mockBrewCasks, nil)
+				mockBrewCommander.EXPECT().RunBrewInfoJSON("chrome", true).Return(nil, expectedErr)
+
+				casks, err := brewCollector.CollectCasks()
+
+				Expect(err).ToNot(HaveOccurred())
+				Expect(casks).ToNot(ConsistOf(
+					types.BrewCask{Name: "chrome", Version: "125.0"},
+				))
+			})
+		})
+
+		When("successfully collects brew packages", func() {
+			It("returns collected packages", func() {
+				mockBrewCommander.EXPECT().RunBrewListCask().Return(mockBrewCasks, nil)
+				mockBrewCommander.EXPECT().RunBrewInfoJSON("chrome", true).Return(mockBrewInfoMap["chrome"], nil)
+
+				casks, err := brewCollector.CollectCasks()
+				Expect(err).ToNot(HaveOccurred())
+				Expect(casks).To(ConsistOf(
+					types.BrewCask{Name: "chrome", Version: "125.0"},
+				))
+			})
+		})
 	})
 })
 
